71c6abcacdadee8d505d3935d2e14d8a0ba3a8b7
Duy Cao <42382543+caominhduy@users.noreply.github.com>
2020-04-25 00:44:16 -0500

    Add files via upload

--- basics.py
+++ basics.py
@@ -2,12 +2,14 @@ import tensorflow as tf
 import numpy as np
 import cv2 as cv
 import math
+import copy
+from colorama import Fore, Back, Style
 
 """
 This contains the mechanics of a rubik cube
 """
 
-RUBIK_SIZE = 2 # means 2x2x2 rubik cube, modify this to change the size
+RUBIK_SIZE = 3 # means 2x2x2 rubik cube, modify this to change the size
 
 def total_combinations(x): # We assume that there is no rubik cube with size <= 1
     a = math.factorial(7)*3**6
@@ -24,11 +26,11 @@ def init_cube(x):
     left = np.full((x,x), 'o')
     top = np.full((x,x), 'b')
     bottom = np.full((x,x), 'g')
-    cube = top, left, front, right, back, bottom
-    return cube[0]
+    cube = [top, left, front, right, back, bottom]
+    return cube
     # We use Western Color Scheme: White(w), Red(r), Blue(b), Yellow(y), Green(g),
     # and Orange(o)
-    # Example of a complete 2x2x2 rubik cube
+    # A solved 2x2x2 rubik cube looks like this (front face is white):
     #        ______
     #       | b b |
     #       | b b |
@@ -40,13 +42,138 @@ def init_cube(x):
     #       | g g |
     #       -------
 
+def test_cube(x): #RUBIK_SIZE must >= 3
+    cube = init_cube(x)
+    cube[0][0,1], cube[0][1,0], cube[0][1,2], cube[0][2,1] = 'g', 'g', 'g', 'g'
+    cube[1][0,1], cube[1][1,0], cube[1][1,2], cube[1][2,1] = 'r', 'r', 'r', 'r'
+    cube[2][0,1], cube[2][1,0], cube[2][1,2], cube[2][2,1] = 'y', 'y', 'y', 'y'
+    cube[3][0,1], cube[3][1,0], cube[3][1,2], cube[3][2,1] = 'o', 'o', 'o', 'o'
+    cube[4][0,1], cube[4][1,0], cube[4][1,2], cube[4][2,1] = 'w', 'w', 'w', 'w'
+    cube[5][0,1], cube[5][1,0], cube[5][1,2], cube[5][2,1] = 'b', 'b', 'b', 'b'
+    return cube
 
-#def visualization(s):
+def turn(x, rubik): # including 18 basic rotations of a rubik cube
+
+    copied = copy.deepcopy(rubik)
+
+    if x == 'l': # rotate left face clockwisely in 90 degrees
+        for i in range(3):
+            rubik[1] = np.rot90(rubik[1])
+        rubik[0][:,0] = np.flipud(copied[4])[:,RUBIK_SIZE-1]
+        rubik[2][:,0] = copied[0][:,0]
+        rubik[5][:,0] = copied[2][:,0]
+        rubik[4][:,RUBIK_SIZE-1] = np.flipud(copied[5])[:,0]
+
+    if x == 'u': # rotate top face clockwisely in 90 degrees
+        for i in range(3):
+            rubik[0] = np.rot90(rubik[0])
+        rubik[1][0,:] = copied[2][0,:]
+        rubik[2][0,:] = copied[3][0,:]
+        rubik[3][0,:] = copied[4][0,:]
+        rubik[4][0,:] = copied[1][0,:]
+
+    if x == 'r': # rotate right face closewisely in 90 degrees
+        for i in range(3):
+            rubik[3] = np.rot90(rubik[3])
+        rubik[0][:,RUBIK_SIZE-1] = copied[2][:,RUBIK_SIZE-1]
+        rubik[2][:,RUBIK_SIZE-1] = copied[5][:,RUBIK_SIZE-1]
+        rubik[5][:,RUBIK_SIZE-1] = np.flipud(copied[4])[:,0]
+        rubik[4][:,0] = np.flipud(copied[0])[:,RUBIK_SIZE-1]
+
+    if x == 'f': # rotate front face closewisely in 90 degrees
+        for i in range(3):
+            rubik[2] = np.rot90(rubik[2])
+        rubik[0][RUBIK_SIZE-1,:] = np.fliplr(np.rot90(copied[1]))[0,:]
+        rubik[1][:,RUBIK_SIZE-1] = np.flipud(np.rot90(copied[5]))[:,0]
+        rubik[3][:,0] = np.flipud(np.rot90(copied[0]))[:,RUBIK_SIZE-1]
+        rubik[5][0,:] = np.fliplr(np.rot90(copied[3]))[RUBIK_SIZE-1,:]
+
+    if x == 'd': # rotate bottom face closewisely in 90 degrees
+        for i in range(3):
+            rubik[5] = np.rot90(rubik[5])
+        rubik[1][RUBIK_SIZE-1,:] = copied[4][RUBIK_SIZE-1,:]
+        rubik[2][RUBIK_SIZE-1,:] = copied[1][RUBIK_SIZE-1,:]
+        rubik[3][RUBIK_SIZE-1,:] = copied[2][RUBIK_SIZE-1,:]
+        rubik[4][RUBIK_SIZE-1,:] = copied[3][RUBIK_SIZE-1,:]
+
+    if x == 'b': # rotate back face closewisely in 90 degrees
+        for i in range(3):
+            rubik[4] = np.rot90(rubik[4])
+        rubik[0][0,:] = np.rot90(copied[3])[0,:]
+        rubik[1][:,0] = np.rot90(copied[0])[:,0]
+        rubik[3][:,RUBIK_SIZE-1] = np.rot90(copied[5])[:,RUBIK_SIZE-1]
+        rubik[5][RUBIK_SIZE-1,:] = np.rot90(copied[1])[RUBIK_SIZE-1,:]
+
+    if x == 'u2': # rotate top face clockwisely in 90 degrees TWICE
+        for f in range(2):
+            turn('u',rubik)
+
+    if x == 'd2': # rotate bottom face clockwisely in 90 degrees TWICE
+        for f in range(2):
+            turn('d',rubik)
+
+    if x == 'r2': # rotate right face clockwisely in 90 degrees TWICE
+        for f in range(2):
+            turn('r',rubik)
+
+    if x == 'f2': # rotate front face clockwisely in 90 degrees TWICE
+        for f in range(2):
+            turn('f',rubik)
+
+    if x == 'l2': # rotate left face clockwisely in 90 degrees TWICE
+        for f in range(2):
+            turn('l',rubik)
+
+    if x == 'b2': # rotate back face clockwisely in 90 degrees TWICE
+        for f in range(2):
+            turn('b',rubik)
+
+    if x == 'ur': # rotate top face COUNTERCLOCKWISELY in 90 degrees
+        for f in range(3):
+            turn('u',rubik)
+
+    if x == 'dr': # rotate bottom face COUNTERCLOCKWISELY in 90 degrees
+        for f in range(3):
+            turn('d',rubik)
+
+    if x == 'fr': # rotate front face COUNTERCLOCKWISELY in 90 degrees
+        for f in range(3):
+            turn('f',rubik)
+
+    if x == 'br': # rotate back face COUNTERCLOCKWISELY in 90 degrees
+        for f in range(3):
+            turn('b',rubik)
+
+    if x == 'lr': # rotate left face COUNTERCLOCKWISELY in 90 degrees
+        for f in range(3):
+            turn('l',rubik)
+
+    if x == 'rr': # rotate right face COUNTERCLOCKWISELY in 90 degrees
+        for f in range(3):
+            turn('r',rubik)
+    return rubik
+
+def visualizer(rubik,size):
+    empty_matrix = np.full((size, size), ' ')
+    for i in range(3):
+        first = np.hstack((empty_matrix, rubik[0], empty_matrix, empty_matrix))
+        second = np.hstack((rubik[1], rubik[2], rubik[3], rubik[4]))
+        third = np.hstack((empty_matrix, rubik[5], empty_matrix))
+    print(first)
+    print(second)
+    print(third)
 
 def main():
     x=total_combinations(RUBIK_SIZE)
     print('There are total ' + str(x) + ' combinations!')
-    y = init_cube(RUBIK_SIZE)
-    print(y)
+    #y = init_cube(RUBIK_SIZE)
+    test = test_cube(RUBIK_SIZE)
+    before = copy.deepcopy(test)
+    print("BEFORE")
+    visualizer(before, RUBIK_SIZE)
+    after = turn('rr', test)
+    print("AFTER")
+    print("")
+    visualizer(after, RUBIK_SIZE)
 
 main()
